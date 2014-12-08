6a5863679af7139b954d0b26e5b8223bf1965575
Chris Rolfs <chris@chrisrolfs.com>
2014-12-08 09:25:50 -0700

    update for server only

--- test.js
+++ test.js
@@ -15,15 +15,37 @@ app
 
 if(app.server){
   externalip(function(err, ip) {
-    ip = ip.trim();
-    // start a socket server
-    console.log(err, ip);
-    // server = require('http').createServer().listen(app.port, ip);
-    io = require('socket.io').listen(app.port)//.set('log level', 3);
-    io.sockets.on('connection', function(socket){
-      console.log('connection');
+      ip = ip.trim();
+      // start a socket server
+      myColor = new color().toHex().value;
+      myUsername = app.user;
+      // addMessage('Server listening on ' + ip + ':' + app.port, 'server', serverColor);
+      server = require('http').createServer().listen(app.port, '71.56.216.152');
+      io = require('socket.io').listen(server);//.set('log level', 0);
+      io.sockets.on('connection', function(socket){
+        // io.emit('message', 'Client connected');
+        // addMessage('Connection attempt....');
+        var c = new color().toHex().value;
+        socket.emit('setup', {color: c});
+
+        var username = '';
+
+        socket.on('ready', function(data){
+          username = data.name;
+          io.emit('message', {user: 'server', message: username + ' has connected', color: serverColor});
+          // addMessage(username + ' has connected', 'server', serverColor);
+        });
+
+        socket.on('message', function(data){
+          addMessage(data.message, data.user, data.color);
+        });
+
+        socket.on('disconnect', function() {
+          io.emit('message', {user: 'server', message: username + ' has disconnected', color: serverColor});
+          // addMessage(username + ' has disconnected', 'server', serverColor);
+        });
+      });
     });
-  });
 } else {
   io = require('socket.io-client').connect(app.address);
   // addMessage('Connecting to ' + app.address);
