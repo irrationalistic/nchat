bbf07a3fa14cec60fa778ecc02f7f05e538c7922
Chris Rolfs <chris@chrisrolfs.com>
2014-12-11 00:37:27 -0700

    Added osx notification system

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 var socketio = require('socket.io-client');
 var _ = require('underscore');
+var notifier = require('node-notifier');
 
 module.exports = function(myData, address, display){
   var io = socketio.connect(address);
@@ -13,7 +14,26 @@ module.exports = function(myData, address, display){
   });
 
   io.on('message', function(data){
-    display.addMessage(data.message, data.name, data.color);   
+    display.addMessage(data.message, data.name, data.color);
+
+    var timeMet = false;
+    setTimeout(function(){
+      if(!timeMet){
+        notifier.notify({
+          title: 'Message',
+          message: 'Message from ' + data.name
+        });
+      }
+    }, 500);
+    var res = require('child_process').exec('osascript utilities/osx-active-app.scpt');
+    display.addMessage(res, 't', 'f00');
+    res.stdout.on('data', function (d) {
+      var rex = new RegExp('nchat(\\s+)(-[s|u|f|p])');
+      if(rex.test(d)){
+        timeMet = true;
+      }
+    });
+    
   });
 
   io.on('users', function(users){
